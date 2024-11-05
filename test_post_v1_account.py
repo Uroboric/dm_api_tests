4d3a8841118127258abb75c8b5c531e1e9ee53ae
osipov.a <osipov.workmail@gmail.com>
2024-11-06 03:15:52 +0500

    1.add assertions 2.add receiving activation token code

--- test_post_v1_account.py
+++ test_post_v1_account.py
@@ -1,9 +1,12 @@
+import pprint
+from json import loads
+
 import requests
 
 
 def test_post_v1_account():
-    # Регистрация пользователя
-    login = 'theodor_crow'
+    # # Регистрация пользователя
+    login = 'theodor_crow9'
     email = f'{login}@mail.com'
     password = 'strongpassword'
     json_data = {
@@ -15,27 +18,42 @@ def test_post_v1_account():
     response = requests.post('http://5.63.153.31:5051/v1/account', json=json_data)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 201, f"User can't created{response.json()}"
 
     # Получить письма из почтового сервера,
     params = {
-        'limit': '50',
+        'limit': '10',
     }
 
     response = requests.get('http://5.63.153.31:5025/api/v2/messages', params=params, verify=False)
     print(response.status_code)
     print(response.text)
+    # pprint.pprint(response.json())
+    assert response.status_code == 200, "Messages cannot be sent"
+
     # Получить активационный токен
+    token = None
+    for item in response.json()['items']:
+        user_data = loads(item['Content']['Body'])
+        user_login = user_data['Login']
+        if user_login == login:
+            print(user_login)
+            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
+            print(token)
+            break
+    assert token is not None, f"Token for user {login}, cannot be received"
 
     # Активация пользователя
     headers = {
         'accept': 'text/plain',
     }
 
-    response = requests.put('http://5.63.153.31:5051/v1/account/769d80ac-e663-4e53-b7e7-fe34df28a53a', headers=headers)
+    response = requests.put(f'http://5.63.153.31:5051/v1/account/{token}', headers=headers)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 200, "User cannot be activated"
 
-    # Авторизоваться
+    # # Авторизоваться
     json_data = {
         'login': login,
         'password': password,
@@ -45,3 +63,4 @@ def test_post_v1_account():
     response = requests.post('http://5.63.153.31:5051/v1/account/login', json=json_data)
     print(response.status_code)
     print(response.text)
+    assert response.status_code == 200, "The user cannot log in"
