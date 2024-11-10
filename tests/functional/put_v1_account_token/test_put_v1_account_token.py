c8e69596e92c8386d80fd2dab4a2a46135284b10
Uroboric <osipov.workmail@gmail.com>
2024-11-10 23:48:51 +0500

    howmework 1 - fixes

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -5,12 +5,12 @@ from dm_api_account.apis.account_api import AccountApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 
 
-def test_post_v1_account():
+def test_put_v1_account_token():
     # Initialization
     account_api = AccountApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
 
-    # # Регистрация пользователя
+    # Регистрация пользователя
     fake = Faker()
     login = "tst_acc" + str(fake.random_int(min=1, max=9999))
     email = f'{login}@mail.com'
@@ -20,29 +20,30 @@ def test_post_v1_account():
         'email': email,
         'password': password,
     }
-
     response = account_api.post_v1_account(json_data=json_data)
     print(f'post_v1_account | status code: {response.status_code}')
     print(f'post_v1_account | response text{response.text}')
-    assert response.status_code == 201, f"User can't created{response.json()}"
+    assert response.status_code == 201, f'User is not created! {response.json()}'
 
-    # Получить письма из почтового сервера,
+    # Получить письма из почтового сервера
     response = mailhog_api.get_api_v2_messages()
     print(f'get_api_v2_messages | status code: {response.status_code}')
     print(f'get_api_v2_messages | response text{response.text}')
-    # pprint.pprint(response.json())
-    assert response.status_code == 200, "Messages cannot be sent"
+    assert response.status_code == 200, 'Email does not received!'
 
     # Получить активационный токен
     token = get_activation_token_by_login(login, response)
-    print(f'Activation token : {token}')
-    assert token is not None, f"Token for user {login}, cannot be received"
+    print(f'Activation token  : {token}')
+    assert token is not None, f'Token for user {login} does not received!'
+
+    # Активация пользователя
+    response = account_api.put_v1_account_token(token=token)
+    print(f'put_v1_account_token | status code: {response.status_code}')
+    print(f'put_v1_account_token | response text{response.text}')
+    assert response.status_code == 200, 'User does not activated!'
 
 
-def get_activation_token_by_login(
-        login,
-        response
-):
+def get_activation_token_by_login(login, response):
     token = None
     for item in response.json()['items']:
         user_data = loads(item['Content']['Body'])
