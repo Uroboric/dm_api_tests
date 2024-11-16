cdc1a79b73c33c8dd2020790764935ad2c1eaa64
Uroboric <osipov.workmail@gmail.com>
2024-11-16 23:57:28 +0500

    Add decorator

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -1,11 +1,28 @@
+import time
 from json import loads
 
 from services.dm_api_account import DMApiAccount
 from services.api_mailhog import MailHogApi
 
 
-class AccountHelper:
+def retryer(function):
+    def wrapper(*args, **kwargs):
+        token = None
+        count = 0
+        while token is None:
+            print(f'Попытка получения токена #{count}')
+            token = function(*args, **kwargs)
+            count += 1
+            if count == 5:
+                raise AssertionError('Превышено количество попыток получения активационного токена')
+            if token:
+                return token
+            time.sleep(1)
 
+    return wrapper
+
+
+class AccountHelper:
     auth_token = None
 
     def __init__(self, dm_account_api: DMApiAccount, mailhog: MailHogApi):
@@ -24,10 +41,10 @@ class AccountHelper:
         return response
 
     def activate_user(self, login: str):
-        response = self.mailhog.mailhog_api.get_api_v2_messages()
-        assert response.status_code == 200, 'Email does not received!'
+        # response = self.mailhog.mailhog_api.get_api_v2_messages()
+        # assert response.status_code == 200, 'Email does not received!'
 
-        token = self.get_activation_token_by_login(login=login, response=response)
+        token = self.get_activation_token_by_login(login=login)
         assert token is not None, f'Token for user {login} does not received!'
 
         response = self.dm_account_api.account_api.put_v1_account_token(token=token)
@@ -59,13 +76,14 @@ class AccountHelper:
         response = self.dm_account_api.login_api.delete_v1_account_login(auth_token=auth_token)
         assert response.status_code == 204, 'User is not unauthorized!'
 
-    @staticmethod
-    def get_activation_token_by_login(login, response):
+    @retryer
+    def get_activation_token_by_login(self, login):
         token = None
+        response = self.mailhog.mailhog_api.get_api_v2_messages()
+        assert response.status_code == 200, 'Email does not received!'
         for item in response.json()['items']:
             user_data = loads(item['Content']['Body'])
             user_login = user_data['Login']
             if user_login == login:
                 token = user_data['ConfirmationLinkUrl'].split('/')[-1]
         return token
-
