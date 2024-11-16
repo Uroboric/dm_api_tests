77d764ab7eb94c20d596fde46245fec52b3fa8c6
Uroboric <osipov.workmail@gmail.com>
2024-11-17 01:20:28 +0500

    Add fixture

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,4 +1,7 @@
+from collections import namedtuple
 from json import loads
+
+import pytest
 from faker import Faker
 
 from helpers.account_helper import AccountHelper
@@ -20,22 +23,41 @@ structlog.configure(
 )
 
 
-def test_post_v1_account():
-    # Initialization
-    # 1.Создали конфигурацию
+@pytest.fixture
+def mailhog_api():
     mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_client = MailHogApi(configuration=mailhog_configuration)
+    return mailhog_client
+
+
+@pytest.fixture
+def account_api():
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
+    account = DMApiAccount(configuration=dm_api_configuration)
+    return account
 
-    # 2. with configuration parametrize our Facades(they join our mini apis)
-    dm_api = DMApiAccount(configuration=dm_api_configuration)
-    mailhog = MailHogApi(configuration=mailhog_configuration)
 
-    # 3. With our Facades(our 2 services - dm create and mailhog MEGA Facade)
-    account_helper = AccountHelper(dm_account_api=dm_api, mailhog=mailhog)
+@pytest.fixture
+def account_helper(account_api, mailhog_api):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
 
-    # Регистрация пользователя
+
+@pytest.fixture()
+def prepare_user():
     fake = Faker()
     login = "tst_account_" + str(fake.random_int(min=1, max=9999))
     email = f'{login}@mail.com'
     password = 'strongpassword'
+    User = namedtuple('User', ['login', 'email', 'password'])
+    user = User(login=login, email=email, password=password)
+    return user
+
+
+def test_post_v1_account(account_helper, prepare_user):
+    login = prepare_user.login
+    password = prepare_user.password
+    email = prepare_user.email
+
+    # Регистрация пользователя
     account_helper.register_new_user(login=login, password=password, email=email)
