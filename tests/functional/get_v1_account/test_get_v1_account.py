2ed2ef0b9bffe10cce9d46d02c0917cfa3a6418a
Uroboric <osipov.workmail@gmail.com>
2024-12-03 02:21:19 +0500

    Add PyHamcrest

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,5 +1,33 @@
+from dm_api_account.models.user_envelope import UserRole
+from datetime import datetime
+from hamcrest import (assert_that, has_property, starts_with, all_of, not_none, is_, instance_of, has_properties,
+                      equal_to, has_items, greater_than_or_equal_to, greater_than, any_of, close_to,
+                      less_than_or_equal_to)
+
+
 def test_get_v1_account(auth_account_helper):
-    auth_account_helper.get_user_info(validate_response=True)
+    response = auth_account_helper.get_user_info(validate_response=True)
+    assert_that(response.resource, all_of(
+        has_property('login', starts_with('tst_account')),
+        has_property('roles', not_none()),
+        has_property('roles', is_(list)),
+        has_property('roles', has_items(
+                    UserRole.GUEST,
+                    UserRole.PLAYER
+                   )
+        ),
+        has_property('registration', instance_of(datetime)),
+        has_property('rating', has_properties(
+                    {
+                        "enabled": equal_to(True),
+                        "quality": equal_to(0),
+                        "quantity": equal_to(0)
+                    }
+                )
+            ),
+
+        )
+    )
 
 
 def test_get_v1_account_no_auth(account_helper):
