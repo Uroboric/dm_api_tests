5a1c7bb707b56bb44dc77898175c3fed667a52d8
Uroboric <osipov.workmail@gmail.com>
2024-12-10 00:47:08 +0500

    add checker functions for 1. test_get_v1_account.py 2. test_post_v1_account_login.py

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,37 +1,12 @@
-import pytest
-from datetime import datetime
-from hamcrest import (assert_that, has_property, starts_with, all_of, not_none, is_, instance_of, has_properties,
-                      equal_to, has_items)
-
+from checkers.get_v1_account import GetV1Account
 from checkers.http_checkers import check_status_code_http
-from dm_api_account.models.user_envelope import UserRole
 
 
 def test_get_v1_account(auth_account_helper):
-    with check_status_code_http(expected_status_code=200):
-        response = auth_account_helper.get_user_info(validate_response=True)
-    assert_that(response.resource, all_of(
-        has_property('login', starts_with('tst_account')),
-        has_property('roles', not_none()),
-        has_property('roles', is_(list)),
-        has_property('roles', has_items(
-                    UserRole.GUEST,
-                    UserRole.PLAYER
-                   )
-        ),
-        has_property('registration', instance_of(datetime)),
-        has_property('rating', has_properties(
-                    {
-                        "enabled": equal_to(True),
-                        "quality": equal_to(0),
-                        "quantity": equal_to(0)
-                    }
-                )
-            ),
-        )
-    )
+    response = auth_account_helper.get_user_info(validate_response=True)
+    GetV1Account.check_response_by_hamcrest(response)
 
-
+    
 def test_get_v1_account_no_auth(account_helper):
     with check_status_code_http(expected_status_code=401, expected_message='User must be authenticated'):
         account_helper.get_user_info()
--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from hamcrest import assert_that, has_property, starts_with, all_of, instance_of, has_properties, equal_to
 
+from checkers.post_v1_account_login import PostV1AccountLogin
+
 
 def test_post_v1_account_login(account_helper, prepare_user):
     login = prepare_user.login
@@ -10,22 +12,4 @@ def test_post_v1_account_login(account_helper, prepare_user):
 
     account_helper.register_and_activate_user(login=login, password=password, email=email)
     response = account_helper.user_login(login=login, password=password, validate_response=True)
-    assert_that(response, all_of(
-        has_property('resource', has_property('login', starts_with('homer_account'))),
-        has_property('resource', has_property('registration', instance_of(datetime))),
-        has_property(
-            'resource', has_properties(
-                    {
-                        'rating': has_properties(
-                            {
-                                "enabled": equal_to(True),
-                                "quality": equal_to(0),
-                                "quantity": equal_to(0)
-                            }
-                        )
-                    }
-                )
-            )
-        )
-    )
-    print(response)
+    PostV1AccountLogin.check_response_values(response)
